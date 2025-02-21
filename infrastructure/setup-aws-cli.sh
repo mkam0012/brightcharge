e584cf20f125f9a2030672ad4b1045e975584203
Mayank Kamra <mayank22kamra@gmail.com>
2025-02-21 14:56:34 +1100

    Removed large files and updated gitignore

--- infrastructure/setup-aws-cli.sh
+++ infrastructure/setup-aws-cli.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -e
 
-# Install AWS CLI
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
+# Install AWS CLI using apt
+sudo apt-get update && sudo apt-get install -y awscli
 
-# Configure AWS CLI will be done using GitHub Actions secrets
+# AWS CLI will be configured using GitHub Actions secrets
 echo "AWS CLI installed successfully"
